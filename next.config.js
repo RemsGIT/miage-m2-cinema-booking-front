2649f002947d03b1655aea75e730fc69bcb35b95
Remy CASTRO <remycastro27@icloud.com>
2024-01-23 12:08:40 +0100

    swiper + image domain

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "encrypted-tbn3.gstatic.com"
+            }
+        ]
+    },
+}
 
 module.exports = nextConfig
